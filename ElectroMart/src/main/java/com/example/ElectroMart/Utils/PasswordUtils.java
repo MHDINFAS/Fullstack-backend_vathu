a708ee4bd7f5e8df240bd5719b3e32b31d2a765f
MHDINFAS <mohammedinfas2129@gmail.com>
2025-03-06 01:44:48 +0530

    added admin password in PasswordUtils in Utils

--- ElectroMart/src/main/java/com/example/ElectroMart/Utils/PasswordUtils.java
+++ ElectroMart/src/main/java/com/example/ElectroMart/Utils/PasswordUtils.java
@@ -1,8 +1,12 @@
 package com.example.ElectroMart.Utils;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordUtils {
 public static void main(String[] args){
-
+    BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+    String hashedpassword =encoder.encode("Admin#123");
+    System.out.println("Hashed Password: "+hashedpassword);
 }
 
 }
